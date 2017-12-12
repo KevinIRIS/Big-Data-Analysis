616d33e7d0d44d499ae4683c2f1d070679f81147
KevinIRIS <kz899@nyu.edu>
2017-12-12 14:42:45 -0500

    analyze for each borough

---
+++ analysis/count_crime_number_for_each_borough.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from pyspark import SQLContext
+from pyspark import SparkContext
+from datetime import datetime
+import sys
+import os
+import tempfile
+from pyspark.sql.functions import lit,year,col,column,count,when
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: bigram <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    dataframe = sqlContext.load(source="com.databricks.spark.csv", path=sys.argv[1], header=True, inferSchema=True)
+    df = dataframe.groupBy("BORO_NM").count()
+    df.save('count_crime_number_for_each_borough.csv','com.databricks.spark.csv')
+    sc.stop()
