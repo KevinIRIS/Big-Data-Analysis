ae0b4d20f5fffc00f6faab51ffaa4e64dc58a55e
KevinIRIS <kz899@nyu.edu>
2017-12-12 14:11:02 -0500

    udpate shell

--- analysis/script.sh
+++ analysis/script.sh
@@ -1,3 +1,5 @@
 pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_by_day.py cleaned_data
 
 pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_by_day.py cleaned_data
+
+pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_for_each_offense_classification.py cleaned_data
