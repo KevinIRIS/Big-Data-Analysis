30d3e1156710a006387ed89c44953ae30bf4b6d3
KevinIRIS <kz899@nyu.edu>
2017-12-12 14:26:21 -0500

    update shell

--- analysis/script.sh
+++ analysis/script.sh
@@ -3,3 +3,6 @@ pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_by_day
 pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_by_day.py cleaned_data
 
 pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_for_each_offense_classification.py cleaned_data
+
+pyspark --packages com.databricks:spark-csv_2.11:1.2.0 count_crime_number_for_each_offense_level.py cleaned_data
+
