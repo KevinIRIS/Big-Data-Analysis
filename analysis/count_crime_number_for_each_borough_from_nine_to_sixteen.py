dc9390ea36609a2586c3fecdcc71e560267751ec
KevinIRIS <kz899@nyu.edu>
2017-12-15 17:47:32 -0500

    add count crime from 2009 to 2016 used for compare with income plot

---
+++ analysis/count_crime_number_for_each_borough_from_nine_to_sixteen.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from pyspark import SQLContext
+from pyspark import SparkContext
+from datetime import datetime
+import sys
+import os
+import tempfile
+from pyspark.sql.functions import lit,year,col,column,count,when
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: bigram <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    dataframe = sqlContext.load(source="com.databricks.spark.csv", path=sys.argv[1], header=True, inferSchema=True)\
+        .withColumn("year", column("RPT_DT").substr(7,4))
+    boroughs = ('BROOKLYN', 'MANHATTAN', 'BRONX', 'QUEENS', 'STATEN ISLAND')
+    res = dataframe.select("year").withColumnRenamed("year","year_data").distinct()
+    res = res.filter(res["year_data"] >= 2009).filter(res["yaer_data"] <= 2016)
+    for borough in boroughs:
+        df = dataframe.filter(column("BORO_NM") == borough).groupBy("year").count()
+        res = df.join(res, df["year"] == res["year_data"]).drop(df.year)
+    res.save('count_crime_number_for_each_borough_from_nine_to_sixteen.csv', 'com.databricks.spark.csv')
+    sc.stop()
