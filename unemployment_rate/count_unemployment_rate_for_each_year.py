1392411d841dd496a25fede3eb51ca6dbe22ea7a
KevinIRIS <kz899@nyu.edu>
2017-12-15 22:52:04 -0500

    compare unemployment rate vs crime rate

--- unemployment_rate/count_unemployment_rate_for_each_year.py
+++ unemployment_rate/count_unemployment_rate_for_each_year.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import sys
 import os
 import tempfile
-from pyspark.sql.functions import *
+from pyspark.sql.functions import lit,year,col,column,count,when
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: bigram <file>", file=sys.stderr)
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     sc = SparkContext()
     sqlContext = SQLContext(sc)
     dataframe = sqlContext.load(source="com.databricks.spark.csv", path=sys.argv[1], header=True, inferSchema=True)
-    dataframe = dataframe.withColumn("year", column("RPT_DT").substr(7,4)).withColumn("month", column("RPT_DT").substr(0,2))
-    df = dataframe.groupBy("RPT_DT").count()
-    df.save('count_crime_by_day.csv','com.databricks.spark.csv')
+    dataframe = dataframe.select("YEAR","Unemployment Rate").filter((dataframe["YEAR"] >= 2006) & (dataframe["YEAR"] <= 2016))
+    df = dataframe.groupBy("YEAR").agg({"Unemployment Rate":"avg"})
+    df.save('count_unemployment_rate_for_each_year.csv', 'com.databricks.spark.csv')
     sc.stop()
